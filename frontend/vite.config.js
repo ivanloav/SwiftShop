e3642752bf76e166202dff79c8166519bab34962
Iván López Ávila <149825835+ivanloav@users.noreply.github.com>
2024-09-23 13:59:48 +0200

    Add dockerfiles & docker compose

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Esto permite que el servidor escuche en todas las interfaces
+    port: 5000, // Asegúrate de que este puerto coincida con el puerto en docker-compose
+  },
+});
