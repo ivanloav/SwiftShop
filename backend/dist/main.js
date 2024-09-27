a92436905db2c028b3be2e7b15052d2de17c1365
Iván López Ávila <149825835+ivanloav@users.noreply.github.com>
2024-09-27 11:04:23 +0200

    Deploy GitHub Actions EC2

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -5,18 +5,18 @@ const app_module_1 = require("./app.module");
 const swagger_1 = require("@nestjs/swagger");
 const dotenv = require("dotenv");
 async function bootstrap() {
-    dotenv.config();
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    const config = new swagger_1.DocumentBuilder()
-        .setTitle("TFM Mock API")
-        .setDescription("API mockeada para la documentación de proyecto TFM")
-        .setVersion("1.0")
-        .build();
-    const document = swagger_1.SwaggerModule.createDocument(app, config);
-    swagger_1.SwaggerModule.setup("api", app, document);
-    const port = process.env.PORT || 3000;
-    await app.listen(port);
-    console.log(`App is running on: http://localhost:${port}`);
+  dotenv.config();
+  const app = await core_1.NestFactory.create(app_module_1.AppModule);
+  const config = new swagger_1.DocumentBuilder()
+    .setTitle("TFM Mock API")
+    .setDescription("API mockeada para la documentación de proyecto TFM")
+    .setVersion("1.0")
+    .build();
+  const document = swagger_1.SwaggerModule.createDocument(app, config);
+  swagger_1.SwaggerModule.setup("api", app, document);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, "0.0.0.0"); // Escuchar en todas las interfaces
+  console.log(`App is running on: http://localhost:${port}`);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
