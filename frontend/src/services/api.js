d6e0626a3af2da10a131a91fea1a896735b24735
Iván López Ávila <149825835+ivanloav@users.noreply.github.com>
2024-09-28 19:41:25 +0200

    Login with JWT and private routes with token

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_BASE_URL = "http://ec2-54-235-225-199.compute-1.amazonaws.com/api";
-const API_BASE_URL = "http://localhost:3000/api";
+const API_BASE_URL = "http://ec2-54-235-225-199.compute-1.amazonaws.com/api";
+//const API_BASE_URL = "http://localhost:3000/api";
 
 // Get token from localStorage
 const getToken = () => localStorage.getItem("token");
